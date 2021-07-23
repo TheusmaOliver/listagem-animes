021ebf0e850af09fe8e5cd244a0a03abac68d28c
Matheus Rodrigues <matheusrodriguesoliveira273@gmail.com>
2021-07-23 15:17:24 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import qs from 'qs';
+import Pagination from './Pagination';
+import Searchinput from './Searchinput';
+import narutoCorrendo from './img/naruto-corendo.gif'
 import './App.css';
 
+const api = "https://kitsu.io/api/edge"
+const LIMIT = 12
+
+
 function App() {
+  const [info,setInfo] = useState({});
+  const [text, setText] = useState('');
+  const [offset, setOffset] = useState(0);
+
+  useEffect(()=>{
+    setInfo({});
+    
+    const query = {
+      page:{
+        limit: LIMIT,
+        offset
+      }
+    };
+
+    if(text){
+      query.filter = {
+        text
+      };
+    }
+
+    fetch(`${api}/anime?${qs.stringify(query)}`)
+      .then((response)=> response.json())
+      .then((response)=>{
+        setInfo(response);
+      })
+    
+      
+    
+  }, [text,offset]);
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1>Lista de Animes</h1>
+      
+      <Searchinput  
+        value={text} 
+        onChange={(search)=> setText(search)}
+      />
+
+      {(text || !text) && !info.data &&
+        <div className='carregando'> 
+          <span>Carregando...</span>
+          <img src={narutoCorrendo} width='100' alt='gif-naruto'/>
+        </div>
+        
+      }
+      {info.data &&(
+        <ul className="animes-list">
+          {info.data.map((anime)=>(
+            
+            <li key={anime.id}>  
+                         
+              <img src={anime.attributes.posterImage.small} alt={anime.attributes.canonicalTitle}/>
+              <h2>{anime.attributes.canonicalTitle}</h2>
+            
+            </li>
+          ))}
+        </ul>
+      )}
+
+      {info.meta &&(
+
+        <Pagination 
+          limit={LIMIT} 
+          total={info.meta.count} 
+          offset={offset}
+          setOffset={setOffset}
+        />
+      )}
+      
     </div>
   );
 }
